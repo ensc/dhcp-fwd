3422d05d0afc76f9406bc37b13f8f7693e8512d7
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2002-07-04 10:42:33 +0000

    Added a lot of functions, macros and typedefs being erroneously in lib/unix.h

--- src/splint_compat.h
+++ src/splint_compat.h
@@ -18,18 +18,262 @@
 
 #ifndef DHCP_FORWARDER_SPLINT_COMPAT_H
 #define DHCP_FORWARDER_SPLINT_COMPAT_H
-
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
-
 #ifdef S_SPLINT_S
-#  define __socklen_t_defined
-#  define u_int8_t		uint8_t
-#  define u_int16_t		uint16_t
-#  define u_int32_t		uint32_t
+
+
+
+#include "../config.h"
+
+typedef int		__socklen_t;
+typedef __socklen_t	socklen_t;
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define TEMP_FAILURE_RETRY(X)	X
+
+typedef /*@integraltype@*/	bool;
+/*@constant bool false@*/
+/*@constant bool true@*/
+
+typedef unsigned short		u_short;
+typedef uint8_t			u_int8_t;
+typedef uint16_t		u_int16_t;
+typedef uint32_t		u_int32_t;
+
+#include <netinet/ip.h>
+
+#define IP_PKTINFO		2
+
+#define __arr		xxx
+#define PATH_MAX	42
+#define CFG_FILENAME	"42"
+
+/*@constant size_t PATH_MAX=42@*/
+
+
+typedef uint16_t in_port_t; /* An unsigned integral type of exactly 16 bits. */
+typedef uint32_t in_addr_t; /* An unsigned integral type of exactly 32 bits. */
+
+/*@constant int MSG_CTRUNC@*/
+
+/*@constant int AF_INET@*/
+/*@constant int AF_INET6@*/
+/*@constant int AF_PACKET@*/
+
+/*@constant int IPPROTO_IP@*/
+/*@constant int IPPROTO_UDP@*/
+
+/*@constant int SOCK_DGRAM@*/
+/*@constant int SOCK_RAW@*/
+
+/*@constant int SOL_SOCKET@*/
+/*@constant int SOL_IP@*/
+
+/*@constant int SO_BROADCAST@*/
+/*@constant int SO_BINDTODEVICE@*/
+
+/*@constant in_addr_t INADDR_ANY@*/
+/*@constant in_addr_t INADDR_BROADCAST@*/
+
+
+
+
+typedef /*@abstract@*/ fd_set;
+
+extern size_t	CMSG_SPACE(size_t);
+#define CMSG_SPACE(X)		X
+
+typedef /*@integraltype@*/	sa_family_t;
+
+struct cmsghdr {
+  socklen_t cmsg_len;		/* data byte count, including hdr */
+  int cmsg_level;		/* originating protocol */
+  int cmsg_type;		/* protocol-specific type */
+} ;
+
+/*@exposed@*/ unsigned char *CMSG_DATA (/*@sef@*/ struct cmsghdr *) /*@*/ ;
+/*@null@*/ /*@exposed@*/ struct cmsghdr *CMSG_NXTHDR (/*@in@*/struct msghdr *,
+						      /*@in@*/struct cmsghdr *) /*@*/ ;
+/*@null@*/ /*@exposed@*/ struct cmsghdr *CMSG_FIRSTHDR (/*@in@*/struct msghdr *) /*@*/ ;
+
+  /* Internet address. */
+struct in_addr {
+    uint32_t      s_addr;     /* address in network byte order */
+};
+
+struct in6_addr {
+    unsigned char   s6_addr[16];/* IPv6 address */
+};
+
+struct sockaddr {
+    sa_family_t		sa_family;		/* address family */
+    char        	sa_data[];		/* variable length */
+};
+
+struct sockaddr_in {
+    sa_family_t     sin_family; /* address family: AF_INET */
+    uint16_t        sin_port;   /* port in network byte order */
+    struct in_addr  sin_addr;  /* internet address */
+};
+
+
+struct sockaddr_ll {
+  sa_family_t	  sll_family;
+  unsigned short  sll_protocol;
+  int             sll_ifindex;
+  unsigned short  sll_hatype;
+  unsigned char   sll_pkttype;
+  unsigned char   sll_halen;
+  unsigned char   sll_addr[8];
+};
+
+struct sockaddr_in6 {
+    u_int16_t		sin6_family;/* AF_INET6 */
+    u_int16_t		sin6_port;/* port number */
+    u_int32_t		sin6_flowinfo;/* IPv6 flow information */
+    struct in6_addr	sin6_addr;/* IPv6 address */
+    u_int32_t		sin6_scope_id;	/* Scope id (new in 2.4) */
+};
+
+
+struct in_pktinfo {
+  int                   ipi_ifindex;
+  struct in_addr        ipi_spec_dst;
+  struct in_addr        ipi_addr;
+};
+
+struct iovec {
+    /*@dependent@*/
+    void    *iov_base;
+    size_t   iov_len;		/*: maxSet(iov_base) == iov_len@ */
+};
+
+struct msghdr {
+    /*@dependent@*/
+    void         * msg_name;     /* optional address */
+    socklen_t    msg_namelen;    /*: maxSet (msg_name) >= msg_namelen */
+    /*@dependent@*/
+    struct iovec * msg_iov;      /* scatter/gather array */
+    size_t       msg_iovlen;     /*: maxSet (msg_iov) >= msg_iovlen */
+    /*@dependent@*/
+    void         * msg_control;  /* ancillary data, see below */
+    socklen_t    msg_controllen; /*: maxSet (msg_control) >= msg_controllen */
+    int          msg_flags;      /* flags on received message */
+};
+
+
+in_addr_t htonl (in_addr_t hostlong) /*@*/ ;
+in_port_t htons (in_port_t hostshort) /*@*/ ;
+in_addr_t ntohl (in_addr_t netlong) /*@*/ ;
+in_port_t ntohs (in_port_t netshort) /*@*/ ;
+
+int inet_aton(/*@in@*/const char *cp, /*@out@*/struct in_addr *inp);
+/*@observer@*/
+char const *inet_ntoa(/*@in@*/struct in_addr in);
+
+const char *inet_ntop(int af, /*@in@*/const void *src,
+		      /*@out@*//*@returned@*/char *dst, size_t cnt);
+
+
+int gettimeofday(/*@out@*//*@null@*/struct timeval *tv,
+		 /*@out@*//*@null@*/struct timezone *tz);
+
+struct tm *localtime_r(/*@in@*/const time_t *timep,
+		       /*@out@*//*@returned@*/struct tm *result);
+
+
+
+int chroot (/*@notnull@*/ /*@nullterminated@*/ const char *path)
+    /*@modifies internalState, errno@*/
+    /*:errorcode -1:*/ 
+  /*@warn superuser "Only super-user processes may call chroot."@*/ ;
+
+int		socket(int domain, int type, int proto)
+  /*@modifies errno@*/ ;
+
+int		setsockopt(int s, int level, int optname, void const *optval, socklen_t optlen)
+  /*@modifies internalState, errno@*/
+  /*@requires maxRead(optval) >= optlen@*/ ;
+
+int		select(int n, /*@null@*/fd_set *r, /*@null@*/fd_set *w,
+		       /*@null@*/fd_set *e, /*@null@*/struct timeval *t)
+  /*@modifies *r, *w, *e, *t, errno@*/ ;
+
+int		recv(int s, /*@out@*/void *buf, size_t len, int flags)
+  /*@modifies *buf, errno@*/
+  /*@requires maxSet(buf) >= len@*/ ;
+
+
+int		recvfrom(int s, /*@out@*/void *buf, size_t len,
+			 int flags,
+			 /*@out@*//*@null@*/struct sockaddr *from, socklen_t *fromlen)
+  /*@modifies *buf, *from, *fromlen, errno@*/
+  /*@requires maxSet(buf) >= len@*/ ;
+
+int		bind(int sockfd, /*@in@*/struct sockaddr *my_addr, int addrlen)
+  /*@modifies errno, fileSystem@*/ ;
+
+ssize_t
+sendto (int s, const void *msg, size_t len, int flags, const struct sockaddr *to, int tolen)
+  /*@requires maxRead(msg) >= len@*/
+  /*@modifies errno, fileSystem@*/ ;
+
+ssize_t
+sendmsg (int s, const struct msghdr *msg, int flags)
+  /*@modifies errno@*/;
+
+
+#include <sys/select.h>
+
+#undef FD_ZERO
+#undef FD_CLR
+#undef FD_SET
+#undef FD_ISSET
+#undef FD_COPY
+
+extern void
+FD_CLR (int n, fd_set *p)
+  /*@modifies *p@*/;
+
+extern void
+FD_COPY (fd_set *f, /*@out@*/ fd_set *t)
+  /*@modifies *t@*/;
+
+extern int /*@alt lltX_bool@*/
+FD_ISSET (int n, fd_set *p)
+  /*@*/;
+
+extern void
+FD_SET (int n, fd_set *p)
+  /*@modifies *p@*/;
+
+extern void
+FD_ZERO (fd_set /*@out@*/ *p)
+  /*@modifies *p@*/;
+
+
+
+#if 1
+extern ssize_t
+recvmsg(int s, /*@special@*/struct msghdr *msg, int flags)
+    /*:errorcode -1:*/
+    /*@requires (maxSet(msg->msg_iov)+1) >= msg->msg_iovlen
+             /\ (maxSet(msg->msg_iov->iov_base)+1) == msg->msg_iov->iov_len@*/
+    /*@requires dependent msg->msg_iov->iov_base@*/
+    /*@sets msg->msg_namelen, msg->msg_iovlen, msg->msg_controllen@*/
+    /*@modifies *msg->msg_iov->iov_base, *msg->msg_control,
+                msg->msg_controllen, msg->msg_flags, fileSystem, errno@*/
+  ;
+#else
+extern ssize_t
+recvmsg (int s, struct msghdr *msg, int flags)
+	/*@modifies msg->msg_iov->iov_base[], errno, fileSystem@*/;
 #endif
 
+#endif
 #endif
 
   // Local Variables:
